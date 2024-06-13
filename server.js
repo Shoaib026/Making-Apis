2b5b3a75e4744f1b84e57df66d91712eca918cf8
Shoaib <101673132+Shoaib026@users.noreply.github.com>
2024-06-14 04:03:11 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 require('./connection')
 
 
-//This line is copied from the npm packed
+//This line is copied from the npm packages
 var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,20 +15,36 @@ app.use(bodyParser.json())
 
 // know linking the Routers folder & files with server.js
 const studroute = require('./Routers/Student')
-// ".use" is for when I want to call only students it will call all the students from Routes path of studroute.
+// ".use" is for when I want to call only students it will call all the student from Routes path of studroute.
 app.use('/student', validateUser , studroute)
+
+function validateUser(req, res, next) {
+    const secretKey = req.app.get('secretKey');
+    console.log('Secret Key:', secretKey); // Log the secret key to verify it is retrieved
+
+    jwt.verify(req.headers['x-access-token'], secretKey, function(err, decoded) {
+         // req.headers['x-access-token']  This is just the path to enter in the header section  
+         // req.app.get('secretKey') This is the secret key  that we had made it before & it should be secured.
+        if (err) {
+            return res.json({ 'Status': "authenticate", message: err.message });
+        } else {
+            req.body.id = decoded.id; // Assuming the token payload has an 'id' field
+            next();
+        }
+    });
+}
+
 
 
 // "secretkey" is coming from the "userController"
-app.set('secretkey',"I am a developer 12332100")
+// "set" is a method used to store a value
+app.set('secretKey', "I am a developer 12332100");
 
 
 
 // add the "userRouter" to connect the folders:
 const userRouter = require('../node/Routers/userRouter')
 app.use('/user',userRouter)
-
-
 
 
 
